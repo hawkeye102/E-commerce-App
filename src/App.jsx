43fd450a7574a04d8abb51b14830dcd9d12f62e9
hawkeye102 <outofbox973@gmail.com>
2025-02-15 01:05:37 +0530

    added a swiper for brand banners

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import Header from "./components/Header";
 import Slider from "./components/SwiperSlider";
 import Homecatslider from "./components/HomeCatSlider";
 import FreeShip from "./components/freeShipping";
+import Banner from "./components/Banner";
 export default function App() {
   return (
     <div>
       <Header />
       <Slider/>
       <FreeShip/>
-      
+      <Banner/>
 
       <main className="p-6">
        
---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,77 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import './style.css'
+
+const Banner = () => {
+  return (
+    <div className="max-w-[1000px] mx-auto mt-8 shadow-lg rounded-lg overflow-hidden">
+      <Swiper
+        modules={[Navigation, Autoplay]}
+        navigation
+        autoplay={{ delay: 3000 }}
+        loop
+        slidesPerView={4} // Show 3 slides at once
+        spaceBetween={20} // Add spacing between images
+        className="rounded-lg"
+      >
+        {/* Slide 1 */}
+        <SwiperSlide>
+          <img
+            src="bannerImgs/images.jpg"
+            alt="Smartphone Deals"
+            className="w-full h-[200px] object-cover rounded-lg"
+          />
+        </SwiperSlide>
+
+        {/* Slide 2 */}
+        <SwiperSlide>
+          <img
+            src="bannerImgs/7.jpg"
+            alt="Furniture Sale"
+            className="w-full h-[200px] object-cover rounded-lg"
+          />
+        </SwiperSlide>
+
+        {/* Slide 3 */}
+        <SwiperSlide>
+          <img
+            src="bannerImgs/wellness.jpg"
+            alt="Best Audio Deals"
+            className="w-full h-[200px] object-cover rounded-lg"
+          />
+        </SwiperSlide>
+
+        {/* Slide 4 */}
+        <SwiperSlide>
+          <img
+            src="bannerImgs/lewel.png"
+            alt="Latest Shoe Collection"
+            className="w-full h-[200px] object-cover rounded-lg"
+          />
+        </SwiperSlide>
+
+        {/* Slide 5 */}
+        <SwiperSlide>
+          <img
+            src="bannerImgs/tag1.png"
+            alt="Laptops & Accessories"
+            className="w-full h-[200px] object-cover rounded-lg"
+          />
+        </SwiperSlide>
+
+        {/* Slide 6 */}
+        <SwiperSlide>
+          <img
+            src="bannerImgs/tag2.jpg"
+            alt="Home Decor Sale"
+            className="w-full h-[200px] object-cover rounded-lg"
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Banner;
